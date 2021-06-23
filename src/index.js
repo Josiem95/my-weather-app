4813f478d7bdc9b5253d16486be34370621d9248
Josie Mortison <81780952+Josiem95@users.noreply.github.com>
2021-06-22 17:36:01 -0700

    CSS and JS

---
+++ src/index.js
@@ -0,0 +1,90 @@
+let now = new Date();
+
+let currentDay = document.querySelector("#current-day");
+
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let day = weekdays[now.getDay()];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let hour = now.getHours();
+let minute = now.getMinutes();
+if (minute < 0) {
+  minute = `0${minute}`;
+}
+
+currentDay.innerHTML = `${day} ${month} ${date}, ${hour}:${minute}`;
+
+//
+
+function searchCity(city) {
+  let apiKey = "9d57b8dfa7c27b11f72c0eaad45b73c1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "9d57b8dfa7c27b11f72c0eaad45b73c1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#currentTemp").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°`;
+  document.querySelector(
+    "#description"
+  ).innerHTML = `${response.data.description}`;
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `Wind: ${response.data.wind.speed}`;
+  document.querySelector(
+    "#humid"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+let enterCity = document.querySelector("#newLocale");
+enterCity.addEventListener("submit", handleSubmit);
+
+let coordsUrl = document.querySelector("#coordsUrl");
+coordsUrl.addEventListener("click", getCurrentCity);
+
+searchCity("North Vancouver");
